33e4b7518710c1c930f4719738d375b1a36a7192
Mateusz Stulczewski <mstulczewski@gmail.com>
2024-09-30 20:30:00 +0200

    Responsiveness to phones.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,27 @@
 function load(path) {
     $("#product").load(path);
+    toggleMenu();
 }
+
+function toggleMenu() {
+    if($(".navigation").hasClass("active")) {
+        $(".navigation").removeClass("active");
+    } else {
+        $(".navigation").addClass("active");
+    }
+}
+
+$(document).ready(() => {
+    $(".navigation").on("click", function(e) {
+        e.stopPropagation();
+    });
+    $("#menu-icon").on("click", function(e) {
+        e.stopPropagation();
+    });
+
+    $(window).on("click", function() { 
+        if ($(".navigation").hasClass("active")) {
+            $(".navigation").removeClass("active");
+        }
+    });
+});
